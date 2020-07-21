cd4b847e4a8dd65a0d4fe57c50ddb0b694444920
Timothy Sexton <tlsexto@gmail.com>
2020-07-21 10:27:55 -0500

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="Home">
+		<p>Hello world</p>
+	</div>
+</template>
+
+<script>
+export default {
+<<<<<<< Updated upstream
+  name: "Home",
+=======
+	name: "Home"
+>>>>>>> Stashed changes
+};
+</script>
+
+<style lang="scss">
+#Home {
+	img {
+		max-width: 400px;
+	}
+}
+</style>
